9fb6f5bd2da88c55eb6a8a42fbab07389f59ac34
Martin Verzilli <mverzilli@manas.com.ar>
2014-11-05 15:42:32 -0300

    Added rotation command and completed sequence for issue #2. Theoretically it's all we need to solve the issue, but I need to test in a wider area.

--- scala/src/main/scala/ar/com/manas/giskard/Square.scala
+++ scala/src/main/scala/ar/com/manas/giskard/Square.scala
@@ -32,10 +32,10 @@ class Square extends Actor with ActorLogging with AutoLogTag {
 
       forward(0.5f)
 
-      //for (i <- 0 until 3) {
-      //  rotate(90)  
-      //  forward(0.5)
-      // }    
+      for (i <- 0 until 1) {
+        rotate(90)  
+        forward(0.5f)
+      }    
 
       pause(2)
 
@@ -50,16 +50,30 @@ class Square extends Actor with ActorLogging with AutoLogTag {
 
   def forward(meters: Float) = {
     val angularSpeed = 0
-    val frontBackTilt = 0.1f
+    val frontBackTilt = -0.1f
 
-    for (i <- 0 until 500) {
+    for (i <- 0 until 100) {
       drone ! Drone.Move(angularSpeed, frontBackTilt)  
       Thread.sleep(20)
     }
   }
 
   def rotate(degrees: Double) = {
+    /*With current settings:
+        d.setConfigOption(maxAltitude, String.valueOf(Math.round(1.5f * 1000)))
+        d.setConfigOption(maxEULAAngle, twoDForm.format(6f * Math.PI / 180f).replace(',', '.'))
+        d.setConfigOption(maxVertSpeed, String.valueOf(Math.round(1f * 1000)))
+        d.setConfigOption(maxYaw, twoDForm.format(50f * Math.PI / 180f).replace(',', '.'))      
 
+        An angular speed of 1f repeated 200 times once each 20ms yields a 180 degree rotation
+    */
+    val angularSpeed = 0.5f
+    val frontBackTilt = 0
+
+    for (i <- 0 until 200) {
+      drone ! Drone.Move(angularSpeed, frontBackTilt)
+      Thread.sleep(20)
+    }
   }
 
   def land = {
